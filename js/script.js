73fc77b0c797a540c6f683dce6074ecd092cf496
Alexey Kalashnikov <kalashnikov.alexey555@mail.ru>
2017-02-19 03:16:30 +0700

    Джаваскрипт
    Джаваскрипт

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var link = document.querySelector(".map-inner a");
+var popup = document.querySelector(".feedback");
+var close = popup.querySelector(".close");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("login");
+
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("feedback-show");
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
+
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("feedback-show");
+  popup.classList.remove("feedback-error");
+});
+
+
+popup.addEventListener("submit", function(event) {
+  if (!login.value || !email.value) {
+    event.preventDefault();
+    popup.classList.remove("feedback-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("feedback-error");
+  } else {  
+    localStorage.setItem("login", login.value);
+  }
+});
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("feedback-show")) {
+      popup.classList.remove("feedback-show");
+      popup.classList.remove("feedback-error");
+    }
+  }
+});
